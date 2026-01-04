dc35015e572bd18b7a1f2b623c5bff636fcbdd12
Sourav Chhetri <chettrisourav28@gmail.com>
2026-01-04 13:37:19 +0530

    fix docker username

--- catalog-service/src/main/java/com/sourav/microservices/catalog_service/domain/ProductService.java
+++ catalog-service/src/main/java/com/sourav/microservices/catalog_service/domain/ProductService.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class ProductService {
     private final ProductRepository productRepository;
 
